44343d4d9b4333665ce27af84ed7319240c58598
Keun Young Ko <kko@Keuns-MacBook-Air.local>
2019-07-24 03:37:34 -0700

    previously made

---
+++ gobike.R
@@ -0,0 +1,45 @@
+library(rvest)
+library(tidyverse)
+library(data.table)
+library(tidytext)
+library(stringr)
+library(ggplot2)
+library(ggrepel)
+library(shiny)
+library(data.table)
+library(leaflet)
+library(geojsonio)
+library(plotly)
+library(rtweet)
+library(readxl)
+library(corrplot)
+
+go2017 <- read.csv("2017_gobike.csv")
+go201801 <- read_csv("201801-fordgobike-tripdata.csv")
+go201802 <- read_csv("201802-fordgobike-tripdata.csv")
+go201803 <- read_csv("201803-fordgobike-tripdata.csv")
+go201804 <- read_csv("201804-fordgobike-tripdata.csv")
+go201805 <- read_csv("201805-fordgobike-tripdata.csv")
+go201806 <- read_csv("201806-fordgobike-tripdata.csv")
+go201807 <- read_csv("201807-fordgobike-tripdata.csv")
+go201808 <- read_csv("201808-fordgobike-tripdata.csv")
+go201809 <- read_csv("201809-fordgobike-tripdata.csv")
+go201810 <- read_csv("201810-fordgobike-tripdata.csv")
+go201811 <- read_csv("201811-fordgobike-tripdata.csv")
+go201812 <- read_csv("201812-fordgobike-tripdata.csv")
+go201901 <- read_csv("201901-fordgobike-tripdata.csv")
+go201902 <- read_csv("201902-fordgobike-tripdata.csv")
+go201903 <- read_csv("201903-fordgobike-tripdata.csv")
+go201904 <- read_csv("201904-fordgobike-tripdata.csv")
+go201905 <- read_csv("201905-baywheels-tripdata.csv")
+go201906 <- read_csv("201906-baywheels-tripdata.csv")
+
+# age
+age2017.vector <- NULL
+for(i in 06:10){
+  
+}
+
+age2018.vector <- NULL
+
+age2019.vector <- NULL
